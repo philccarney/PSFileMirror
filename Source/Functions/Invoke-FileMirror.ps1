6eb0cf16141522d2391af9477afcf07edc31e6a3
phlcrny <philipccarney@gmail.com>
2019-02-23 21:42:05 +0000

    Restructure based on ShouldProcess blocks; rename params to PS defaults.

--- Source/Functions/Invoke-FileMirror.ps1
+++ Source/Functions/Invoke-FileMirror.ps1
@@ -5,9 +5,9 @@ function Invoke-FileMirror
     TBC - Short description
 .DESCRIPTION
     TBC - Long description
-.PARAMETER Source
+.PARAMETER Path
     TBC
-.PARAMETER Target
+.PARAMETER Destination
     TBC
 .PARAMETER Log
     TBC
@@ -28,11 +28,10 @@ function Invoke-FileMirror
     param
     (
         [Parameter(Mandatory = $True, Position = 0)]
-        [string] $Source,
+        [string] $Path,
 
         [Parameter(Mandatory = $True, Position = 1)]
-        [Alias("Destination")]
-        [string] $Target,
+        [string] $Destination,
 
         [Parameter(Mandatory = $False, Position = 2)]
         [string] $Log,
@@ -53,13 +52,12 @@ function Invoke-FileMirror
         Write-Debug -Message "BEGIN Block"
 
         #region Parameter-handling and Variables
-        $DateFormat = "%y/%m%/%d-%H:%M:%S"
         $StartTime = Get-Date
         Add-LogEntry -Message "PSFileMirror started." -Log $Log
 
         Write-Verbose -Message "Setting file collection parameters."
         $DirSplat = @{
-            Path    = $Source
+            Path    = $Path
             File    = $True
             Recurse = $True
         }
@@ -75,9 +73,8 @@ function Invoke-FileMirror
 
         if ($Files)
         {
-            Add-LogEntry "Files ($($Files.Count)) scraped from source ($Source)" -Log $Log
+            Add-LogEntry "Files ($($Files.Count)) scraped from source ($Path)" -Log $Log
         }
-
         #endregion Parameter-handling and Variables
     }
 
@@ -86,83 +83,153 @@ function Invoke-FileMirror
         Write-Debug -Message "PROCESS Block"
         if ($Files)
         {
+            # Some basic counters to help keep track of what has been done.
             [int] $FilesProcessed = 0
             [int] $FilesToTransfer = 0
             [int] $FilesTransferred = 0
+
             forEach ($File in $Files)
             {
-                if ($PSCmdlet.ShouldProcess($File, "Processing file for transfer"))
+                if ($PSCmdlet.ShouldProcess($File.FullName, "Processing destination path"))
                 {
                     try
                     {
                         Write-Verbose -Message "Processing '$File'."
-                        $ProposedPath = ($File.FullName).Replace($Source, $Target)
+                        $ProposedPath = ($File.FullName).Replace($Path, $Target)
                         $ProposedParentDirectory = Split-Path -Path $ProposedPath -Parent
-                        if (($Fast) -and (Test-Path -LiteralPath $ProposedPath))
+                        if (Test-Path -LiteralPath -Path $ProposedPath)
                         {
-                            # If Fast is specified, we don't care about checking hashes etc; only the existence of the file matters.
-                            Write-Verbose "File already exists in destination. No further action required."
+                            $FileExistsInDestination = $True
                         }
-                        else
+                    }
+                    catch
+                    {
+                        $PSCmdlet.ThrowTerminatingError($_)
+                        Break
+                    }
+                }
+
+                if ($PSCmdlet.ShouldProcess($File.FullName, "Validating destination folder"))
+                {
+                    try
+                    {
+                        if (-not (Test-Path -LiteralPath $ProposedParentDirectory))
                         {
-                            if ((-not ($Fast)) -and (Test-Path -LiteralPath $ProposedPath))
+                            Write-Verbose -Message "Destination folder does not exist. Creating. ($ProposedParentDirectory)"
+                            $ParentDirSplat = @{
+                                Path        = $ProposedParentDirectory
+                                ItemType    = "Directory"
+                                ErrorAction = "Stop"
+                            }
+
+                            New-Item @ParentDirSplat | Out-Null
+                        }
+                    }
+                    catch
+                    {
+                        $PSCmdlet.ThrowTerminatingError($_)
+                        Break
+                    }
+                }
+
+
+                if ($Fast)
+                {
+                    # Logic branch for the Fast run-through
+                    if ($PSCmdlet.ShouldProcess($File.FullName, "Confirm if file exists in destination"))
+                    {
+                        try
+                        {
+                            if ($Null -ne $FileExistsInDestination)
+                            {
+                                # If Fast is specified, we don't care about checking hashes etc; only the existence of the file matters.
+                                Write-Verbose "File already exists in destination. No further action required."
+                                Continue
+                            }
+                        }
+                        catch
+                        {
+                            $PSCmdlet.ThrowTerminatingError($_)
+                            Break
+                        }
+                    }
+                }
+                else
+                {
+                    if ($Null -ne $FileExistsInDestination)
+                    {
+                        if ($PSCmdlet.ShouldProcess($File.FullName, "Process file hashes for source and existing destination file"))
+                        {
+                            try
                             {
                                 # If Fast isn't specified, we need to consider hashes, so we'll gather them before we do anything else.
-                                # I've used SHA256 for now, but I've used MD5 previously. I need to clarify which is preferable for this.
-                                $OriginalFileHash = Get-FileHash -Path $File -Algorithm "SHA256" |
+                                $HashSplat = @{
+                                    Algorithm   = "SHA256"
+                                    ErrorAction = "Stop"
+                                }
+
+                                $OriginalFileHash = Get-FileHash @HashSplat -Path $File |
                                     Select-Object -ExpandProperty "Hash"
 
-                                $DestinationFileHash = Get-FileHash -Path $ProposedPath -Algorithm "SHA256" |
+                                $DestinationFileHash = Get-FileHash @HashSplat -Path $ProposedPath |
                                     Select-Object -ExpandProperty "Hash"
 
                                 if ($DestinationFileHash -ne $OriginalFileHash)
                                 {
                                     Write-Verbose -Message "Hash mismatch: $OriginalFileHash/$DestinationFileHash"
-                                    $HashMismatch = $True
-                                }
-                            }
-
-                            if (-not (Test-Path -LiteralPath $ProposedParentDirectory))
-                            {
-                                Write-Verbose -Message "Destination folder does not exist. Creating. ($ProposedParentDirectory)"
-                                $ParentDirSplat = @{
-                                    Path        = $ProposedParentDirectory
-                                    ItemType    = "Directory"
-                                    ErrorAction = "Stop"
-                                }
-
-                                New-Item @ParentDirSplat | Out-Null
-                            }
-
-                            if ((-not (Test-Path -LiteralPath $ProposedPath)) -or ($HashMismatch))
-                            {
-                                Write-Verbose -Message "Copying '$File' to '$ProposedPath'."
-                                $CopySplat = @{
-                                    LiteralPath = $File
-                                    Destination = $ProposedPath
-                                    Force       = $True
-                                }
-
-                                Copy-Item @CopySplat
-
-                                if ((-not ($Fast)) -and
-                                    ((Get-FileHash -Path $File -Algorithm "SHA256").Hash -eq (Get-FileHash -Path $ProposedPath -Algorithm "SHA256").Hash))
-                                {
-                                    Write-Verbose -Message "Copy completed successfully. Hashes match."
+                                    FileShouldBeCopied = $True
                                 }
                                 else
                                 {
-                                    if (-not ($Fast)) # This *should* capture conditions which are NotFast but failed the comparison - without rerunning it.
-                                    {
-                                        $Message = "Hash comparison failed for '$File'/'$ProposedPath'"
-                                        Write-Warning -Message $Message
-                                        Add-LogEntry -Message $Message -Log $Log
-                                    }
+                                    Write-Verbose -Message "Hashes match."
+                                }
+                            }
+                            catch
+                            {
+                                $PSCmdlet.ThrowTerminatingError($_)
+                                Break
+                            }
+                        }
+                    }
+                }
+
+                if ($PSCmdlet.ShouldProcess($File, "Processing file for transfer"))
+                {
+                    try
+                    {
+                        if (($Null -ne $FileExistsInDestination) -or (FileShouldBeCopied))
+                        {
+                            $FilesToTransfer ++
+                            Write-Verbose -Message "Copying '$File' to '$ProposedPath'."
+                            $CopySplat = @{
+                                LiteralPath = $File
+                                Destination = $ProposedPath
+                                Force       = $True
+                                ErrorAction = "Stop"
+                            }
+
+                            Copy-Item @CopySplat
+
+                            if ((-not ($Fast)) -and
+                                ((Get-FileHash -Path $File -Algorithm "SHA256").Hash -eq (Get-FileHash -Path $ProposedPath -Algorithm "SHA256").Hash))
+                            {
+                                Write-Verbose -Message "Copy completed successfully. Hashes match."
+                                $FilesTransferred ++
+                            }
+                            else
+                            {
+                                if (-not ($Fast)) # This *should* capture conditions which are NotFast but failed the comparison - without rerunning it.
+                                {
+                                    $Message = "Hash comparison failed for '$File'/'$ProposedPath'"
+                                    Write-Warning -Message $Message
+                                    Add-LogEntry -Message $Message -Log $Log
+                                }
+                                else
+                                {
+                                    $FilesTransferred ++
                                 }
                             }
                         }
-
-                        $FilesProcessed ++
                     }
                     catch
                     {
@@ -170,6 +237,7 @@ function Invoke-FileMirror
                     }
                     finally
                     {
+                        $FilesProcessed ++
                         Add-LogEntry -Message "Processed: $File" -Log $Log
                     }
                 }
@@ -180,5 +248,10 @@ function Invoke-FileMirror
     END
     {
         Write-Debug -Message "END Block"
+        $ElapsedTime = New-TimeSpan -Start $StartTime -End (Get-Date)
+        Add-LogEntry -Message "Elapsed time: $ElapsedTime" -Log $Log
+        Add-LogEntry -Message "Files processed: $FilesProcessed" -Log $Log
+        Add-LogEntry -Message "Files to transfer: $FilesToTransfer" -Log $Log
+        Add-LogEntry -Message "Files transferred successfully: $FilesTransferred" -Log $Log
     }
 }
