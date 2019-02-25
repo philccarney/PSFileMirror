8b480f593d47b2a7242f5f4e14db1cd049a5d981
phlcrny <philipccarney@gmail.com>
2019-02-25 18:02:43 +0000

    Add basic module structure tests.

---
+++ Tests/PSFileMirror.Module.Tests.ps1
@@ -0,0 +1,32 @@
+$ModuleName = "$((Split-Path -Path $PSScriptRoot) -split "\\" | Select-Object -Last 1).psd1"
+$ModuleManifestPath = Join-Path -Path $(Split-Path -Path $PSScriptRoot) -ChildPath $ModuleName
+
+Describe "Module Manifest Tests" -Tag "Build" {
+
+    BeforeAll {
+
+        $ModuleManifest = Test-ModuleManifest -Path $ModuleManifestPath
+    }
+
+    It "Passes 'Test-ModuleManifest'" {
+
+        $ModuleManifest | Should -Not -BeNullOrEmpty
+        $? | Should -Be $True
+    }
+
+    It "Has a module description" {
+
+        $ModuleManifest.Description | Should -Not -BeNullOrEmpty
+    }
+
+    It "Has a valid version" {
+
+        $ModuleManifest.Version.ToString() | Should -Not -BeNullOrEmpty
+        $ModuleManifest.Version.ToString() | Should -match "\d+[.]\d+[.]\d+"
+    }
+
+    It "Has exported functions" {
+
+        $ModuleManifest.ExportedFunctions | Should -Not -BeNullOrEmpty
+    }
+}
